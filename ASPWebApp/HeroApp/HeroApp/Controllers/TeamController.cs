a779f8da7f6533b188ec6858170aacc1fccf48a8
Dharmindra Sean Kaila <dharmindra.s.kaila@avanade.com>
2021-04-28 21:48:12 +0100

    The ablility to add a team was added into the soulition.

--- ASPWebApp/HeroApp/HeroApp/Controllers/TeamController.cs
+++ ASPWebApp/HeroApp/HeroApp/Controllers/TeamController.cs
@@ -1,4 +1,5 @@
 ﻿using HeroApp.Data;
+using HeroApp.Models.Binding;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using System;
@@ -8,17 +9,44 @@ using System.Threading.Tasks;
 
 namespace HeroApp.Controllers
 {
+    [Route("[Controller]")]
     public class TeamController : Controller
     {
+        
         private readonly ApplicationDBContext dbContext;
         public TeamController(ApplicationDBContext applicationDbContext)
         {
             dbContext = applicationDbContext;
         }
-        public IActionResult Index()
+
+
+        [Route("")]
+        public IActionResult Index() //Displays all the records in the Teams table. 
         {
             var allTeams = dbContext.Teams.ToList();
-            return View(allTeams);
+            return View(allTeams);  
+        }
+
+
+        [Route("AddTeam")]
+        public IActionResult AddTeam() 
+        {
+            return View();
+        }
+        [HttpPost("AddTeam")]
+        public IActionResult AddTeam(AddTeamBindingModel bindingModel) //Adds a record to the teams table. 
+        {
+            var TeamValues = new Team
+            {
+                TeamName = bindingModel.TeamName,
+                City = bindingModel.City,
+                EstablishedDate = bindingModel.EstablishedDate,
+                RivalTeam = bindingModel.RivalTeam,
+                Logo = "https://www.pngitem.com/pimgs/m/5-50673_superman-logo-vector-blank-superman-logo-png-transparent.png", //Add if statement for emptyLogo. 
+            };
+            dbContext.Teams.Add(TeamValues); //Adds the record. 
+            dbContext.SaveChanges();  //Saves the changes made to the record
+            return RedirectToAction("Index"); //Takes you back to the Teams page.
         }
 
 
---
+++ ASPWebApp/HeroApp/HeroApp/Models/Binding/AddTeamBindingModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace HeroApp.Models.Binding
+{
+    public class AddTeamBindingModel
+    {
+        public int TeamID { get; set; }
+        public string TeamName { get; set; }
+        public string City { get; set; }
+        public string RivalTeam { get; set; }
+        public string EstablishedDate { get; set; }
+        public string Logo { get; set; }
+    }
+}
